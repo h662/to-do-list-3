a4e97a14c8b6378bb74e4da36faeb7048293c658
h662 <h662hong@gmail.com>
2024-05-29 15:36:52 +0900

    투두 완료

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -1,15 +1,35 @@
 import { Button, Flex, Text } from "@chakra-ui/react";
-import { FC } from "react";
+import { Dispatch, FC, SetStateAction } from "react";
 import { FiEdit3, FiTrash2 } from "react-icons/fi";
 
 interface TodoCardProps {
   todo: ITodo;
+  todos: ITodo[];
+  setTodos: Dispatch<SetStateAction<ITodo[]>>;
 }
 
-const TodoCard: FC<TodoCardProps> = ({ todo }) => {
+const TodoCard: FC<TodoCardProps> = ({ todo, todos, setTodos }) => {
+  const onClickIsDone = () => {
+    const temp = todos.map((v) => {
+      if (v.id === todo.id) {
+        return { id: todo.id, content: todo.content, isDone: !todo.isDone };
+      } else {
+        return v;
+      }
+    });
+
+    setTodos(temp);
+  };
+
   return (
     <Flex bgColor="white" px={4} py={2} rounded="lg" gap={1}>
-      <Text fontSize={20} w={48} isTruncated={true}>
+      <Text
+        fontSize={20}
+        w={48}
+        isTruncated={true}
+        textDecorationLine={`${todo.isDone ? "line-through" : ""}`}
+        onClick={onClickIsDone}
+      >
         {todo.content}
       </Text>
       <Button colorScheme="blue">
