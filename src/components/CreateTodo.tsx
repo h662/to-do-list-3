29aad9040faeced0d7e0bb24e314741709bed689
h662 <h662hong@gmail.com>
2024-05-29 16:30:35 +0900

    투두 업데이트

--- src/components/CreateTodo.tsx
+++ src/components/CreateTodo.tsx
@@ -8,7 +8,7 @@ interface CreateTodoProps {
 
 const CreateTodo: FC<CreateTodoProps> = ({ todos, setTodos }) => {
   const [currentTodoId, setCurrentTodoId] = useState<number>(
-    todos[todos.length - 1].id
+    todos[todos.length - 1]?.id
   );
   const [content, setContent] = useState<string>("");
 
